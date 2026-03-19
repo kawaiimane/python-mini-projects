f7263d0c3be17d6a9d24e534ca330835616eaa27
kawaiimane <269561590+kawaiimane@users.noreply.github.com>
2026-03-19 17:46:30 -0400

    Add digital clock project

---
+++ digital-clock/digital_clock.py
@@ -0,0 +1,25 @@
+import time
+import tkinter as tk
+
+
+window = tk.Tk()
+window.geometry("150x30")
+
+label = tk.Label(window, text="")
+label.pack()
+
+
+def clock():
+    label.config(text=time.strftime("%I:%M:%S %p"))
+    window.after(1000, clock)
+
+
+clock()
+
+window.update_idletasks()
+width = window.winfo_width()
+height = window.winfo_height()
+x = (window.winfo_screenwidth() // 2) - (width // 2)
+y = (window.winfo_screenheight() // 2) - (height // 2)
+window.geometry(f"{width}x{height}+{x}+{y}")
+window.mainloop()
