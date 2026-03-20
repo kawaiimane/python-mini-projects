21720e7c855ab541e8f1135b964fc948f17cfb7b
kawaiimane <269561590+kawaiimane@users.noreply.github.com>
2026-03-20 16:52:29 -0400

    Update encrypt_decrypt_text.py

--- encrypt-decrypt-text/encrypt_decrypt_text.py
+++ encrypt-decrypt-text/encrypt_decrypt_text.py
@@ -7,27 +7,25 @@ key = get_random_bytes(32)
 
 
 def encrypt(plain_text, key):
-
+    
     cipher_encrypt = AES.new(key, AES.MODE_CBC)
     padded = pad(plain_text.encode("utf-8"), AES.block_size)
     encrypt = cipher_encrypt.encrypt(padded)
     encoded = b64encode(encrypt)
-
+    
     return encoded, cipher_encrypt.iv
 
 
-encoded, iv = encrypt("twilight is the best movie ever", key)
-print(encoded, iv)
-
-
 def decrypt(encoded, iv):
-
+    
     cipher_decrypt = AES.new(key, AES.MODE_CBC, iv)
     decoded = b64decode(encoded)
     decrypt = cipher_decrypt.decrypt(decoded)
     unpadded = unpad(decrypt, AES.block_size).decode("utf-8")
-
+    
     return unpadded
 
 
+encoded, iv = encrypt("twilight is the best movie ever", key)
+print(encoded, iv)
 print(decrypt(encoded, iv))
